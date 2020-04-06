aa04fc357e9d6f51780922c17ddf5ca00d8c229a
fco_s <fco_s@DESKTOP-FI459OL>
2020-04-06 11:49:20 -0600

    Traslado a la carpeta de Prueba.

---
+++ src/Declaradores_Operadores.java
@@ -0,0 +1,30 @@
+
+public class Declaradores_Operadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float a = 5F;
+		
+		float b;
+		
+		b = 7F;
+		
+		float c = b / a;
+		
+		//c -= 6;
+		
+		final double apulgadas = 2.54;
+		
+		double cm = 6;
+		
+		double resultado = cm / apulgadas;
+		
+		int js, ia, ja;
+		
+
+
+		System.out.println("En " + cm + " cm hay " + resultado + " pulgadas");
+	}
+
+}
